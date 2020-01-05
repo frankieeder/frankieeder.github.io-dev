bd7b98dc6d9216dbed99b40a660ce5a6c49a0a9a
Frankie Eder <frankieeder129@gmail.com>
2020-01-04 22:08:13 -0800

    Added updated filtering functionality .

--- manip.js
+++ manip.js
@@ -5,17 +5,12 @@ var filters = new Set();
 
 function filterContent(filter) {
     // shows all content if you pass in ''
+    var refresh = false;
     if (filter == '') {
-        filters = new Set();
-    } else if (filters.has(filter)) {
-        filters.delete(filter);
-    } else {
-        filters.add(filter);
+        refresh = true;
     }
-    
-    console.log(filters);
 	for (var i = 0; i < content.length; i++) {
-		if (classInFilters(content[i])) {
+		if (refresh || content[i].classList.contains(filter)) {
 			content[i].style.display = "inline-block";
 		} else {
             content[i].style.display = "none";
@@ -23,41 +18,29 @@ function filterContent(filter) {
 	}
 }
 
-function classInFilters(el) {
-    var classes = el.className.split(' ');
-    console.log(classes)
-    var hasAllFilters = true;
-    for (let filter of filters) {
-        console.log(filter);
-        hasAllFilters = hasAllFilters && classes.includes(filter);
-    }
-    return hasAllFilters;
-}
-
-
-function centerButtonText() {
-    //console.log(toCenter);
-    for (var i = 0; i < toCenter.length; i++) {
-        //console.log(toCenter[i].style['height'])
-        toCenter[i].style['lineHeight'] = toCenter[i].style['height'];
-        //console.log(toCenter[i].style['lineHeight'])
-	}
-}
-centerButtonText();
-
 function enableNavigation() {
     var nav = document.getElementsByClassName('nav')[0];
     var dropDowns = nav.getElementsByTagName('ul');
     for (var i = 1; i < dropDowns.length; i++) { // Skip first, main ul
         dropDowns[i].classList.toggle('header_dropdown_ul');
         var adjacentTxtEl = dropDowns[i].parentNode.querySelector('a');
-        console.log(adjacentTxtEl);
         adjacentTxtEl.onclick = function() {
-            console.log(this);
-            filterContent(this.textContent);
             var dropdown = this.parentElement.querySelector("ul");
             dropdown.classList.toggle("active");
             dropdown.classList.toggle("header_dropdown_ul");
+        };
+    }
+    
+    var navOptions = nav.querySelectorAll('li a');
+    for (var i = 0; i < navOptions.length; i++) {
+        navOptions[i].onclick = function() {
+            console.log(this);
+            filterContent(this.textContent);
+            var dropdown = this.parentElement.querySelector("ul");
+            if (dropdown) {
+                dropdown.classList.toggle("active");
+                dropdown.classList.toggle("header_dropdown_ul");
+            }
         };
     }
 }
