27fdb566c32f5aac9ed5675290e39cc9569fd000
Frankie Eder <frankieeder129@gmail.com>
2020-01-06 21:27:18 -0800

    Working coloring.

--- manip.js
+++ manip.js
@@ -15,7 +15,8 @@ var args = getUrlVars();
 
 function filterContent(filter) {
     showContent(filter);
-    updateNav(filter);
+    var hierarchy = updateNav(filter);
+    updateHistory(hierarchy);
 }
 
 function showContent(filter) {
@@ -23,16 +24,21 @@ function showContent(filter) {
         console.log('Setting Default Filter')
         filter = DEFAULT_FILTER;
     }
+    var found = false;
 	for (var i = 0; i < content.length; i++) {
 		if (content[i].classList.contains(filter)) {
 			content[i].style.display = "inline-block";
+            found = true;
 		} else {
             content[i].style.display = "none";
         }
 	}
+    return found;
 }
 
 function updateNav(filter) {
+    // Returns a list representing the heirarchy of the input filter.
+    
     // Close all navigation
     // Inefficient, but should work fine with the limited number of nav items
     var navLis = nav.querySelectorAll('li')
@@ -49,16 +55,26 @@ function updateNav(filter) {
     }
     console.log(targetLi);
     // Expand relevant nav dropdowns if the filter is found
+    var hierarchy = [];
     if (targetLi) {
         console.log(targetLi.tagName);
         // Expand the target and parents
         while (targetLi.tagName == 'LI') {
-            console.log(targetLi);
+            var txt = targetLi.querySelector('a').textContent;
+            console.log(targetLi, txt);
+            hierarchy.unshift(txt);
             targetLi.classList.add('nav_active');
             targetLi.classList.remove('nav_inactive');
             targetLi = targetLi.parentNode.parentNode; // heirarchy parent is two DOM levels up
         }
     }
+    console.log(hierarchy);
+    return hierarchy;
+}
+
+function updateHistory(hierarchy) {
+    var suffix = hierarchy.join("/");
+    window.history.pushState('', hierarchy[hierarchy.length-1], suffix);
 }
 
 function enableNav() {
@@ -91,141 +107,10 @@ function enableNav() {
     }
     console.log('nav anchors after', navItems);
 }
+
 enableNav();
 console.log(args['page']);
 filterContent(args['page']);
-
-/*
-function constructNavDict() {
-    return navDictConstructor(nav);
-}
-function navDictConstructor(root) {
-    var info = {}
-    info['elem'] = root;
-    info['text'] = root.children[0].text;
-    info['children'] = [];
-    var dropList = root.children[1];   
-    console.log(info, dropList);
-    if (dropList) {
-        console.log(dropList.children);
-        for (var i in dropList.children) {
-            console.log(dropList.children[i]);
-            info['children'].push(navDictConstructor(dropList.children[i]));            
-        }
-    }
-    return info;
-}
-var heirarchy = constructNavDict();
-console.log(heirarchy);*/
-
-/* kills hover styles
-function setButtonColors() {
-    var minColor = '#d1e3ff';
-    var maxColor = '#173b73';
-    var buttonTexts = nav.querySelectorAll('li a');
-    console.log(buttonTexts);
-    for (var i in buttonTexts) {
-        var button = buttonTexts[i];
-        var numContent = document.getElementsByClassName(button.text).length;
-        var color = lerpColor(minColor, maxColor, numContent / content.length);
-        console.log(button, button.style, numContent, color);
-        button.style['backgroundColor'] = color;
-    }
-    //setButtonColorsChildren(nav);
-}
-setButtonColors();
-console.log(document.style);/*
-/*
-function setButtonColorsChildren(root) {
-    var maxColor = '#d1e3ff';
-    var minColor = '#173b73';
-    var thisAnchor = root.children[0];
-    var children = root.children[1];
-    for (var i in buttonTexts) {
-        
-    }
-    var thisAnchor = 
-}
-
-function filterContent(filter) {
-    // shows all content if you pass in ''
-    var refresh = false;
-    if (filter == '') {
-        refresh = true;
-    }
-	for (var i = 0; i < content.length; i++) {
-		if (refresh || content[i].classList.contains(filter)) {
-			content[i].style.display = "inline-block";
-		} else {
-            content[i].style.display = "none";
-        }
-	}
-}
-
-function updateNav(filter) {
-    var navDir = filter.split('/');
-    var currEl;    
-    // Update Current URL
-}
-
-function findElementsFromText(root, txt) {
-    return  document.evaluate("//a[contains(., '" + txt + "')]", document, null, XPathResult.ANY_TYPE, null );
-}
-function reconstructNavHierarchyFromLeaf(leaf) {
-    var texts = nav.querySelectorAll('li a');
-    for (var i = 0; i < texts.length; i++) {}
-    
-    return heirarchyReconstructor(nav, leaf, "");
-}
-
-function heirarchyReconstructor(el, key, prefix) {
-    
-    console.log(el);
-    var currText = el.querySelector('a').textContent;
-    if (currText == key) {
-        return key;
-    }
-    var dropDown = el.querySelector('ul')
-    if (dropDown) {
-        for (var child in dropDown.childNodes) {
-            return heirarchyReconstructor(child, key, prefix + currText);
-        } 
-    }
-} */
-/*
-function enableNavigation() {
-    /*var dropDowns = nav.getElementsByTagName('ul');
-    for (var i = 1; i < dropDowns.length; i++) { // Skip first, main ul
-        dropDowns[i].classList.toggle('header_dropdown_ul');
-        var adjacentTxtEl = dropDowns[i].parentNode.querySelector('a');
-        adjacentTxtEl.onclick = function() {
-            var dropdown = this.parentElement.querySelector("ul");
-            dropdown.classList.toggle("active");
-            dropdown.classList.toggle("header_dropdown_ul");
-        };
-    }*\/
-    
-    var navOptions = nav.querySelectorAll('li a');
-    for (var i = 0; i < navOptions.length; i++) {
-        var dropdown = navOptions[i].parentElement.querySelector("ul");
-        if (dropdown) {
-            dropdown.classList.toggle('header_dropdown_ul');
-            navOptions[i].onclick = function () {
-                var dropdown = this.parentElement.querySelector("ul");
-                console.log(this);
-                console.log(dropdown);
-                dropdown.classList.toggle("active");
-                dropdown.classList.toggle("header_dropdown_ul");
-            }
-        } else {
-            navOptions[i].onclick = function() {
-                console.log(this);
-                filterContent(this.textContent);
-            };
-        }
-    }
-}
-enableNavigation();*/
 
 /* https://gist.github.com/rosszurowski/67f04465c424a9bc0dae*/
 function lerpColor(a, b, amount) { 
