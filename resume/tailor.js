b3ee91a3320d7f6b5ec5e522b80bbc6fb654b258
Frankie Eder <frankaeder@gmail.com>
2021-01-18 15:57:52 -0800

    Added skillbuilder.

--- resume/tailor.js
+++ resume/tailor.js
@@ -1,28 +1,74 @@
-// Built from http://www.askyb.com/javascript/load-json-file-locally-by-js-without-jquery/
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    console.log(xobj)
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'default_args.json', true);
-    xobj.onreadystatechange = function () {
-        console.log("inside function:", xobj);
-        if (xobj.readyState == 4 && xobj.status == "200") {
-
-        // .open will NOT return a value but simply returns undefined in async mode so use a callback
-        callback(xobj.responseText);
-
+var default_args = {
+    "filters": [],
+    "skills": [
+        {
+            "skillCategory": "Languages",
+            "limit": 99,
+            "subSkills": {
+                "Python": 10,
+                "SQL": 9,
+                "Snowflake": 9,
+                "Javascript": 8,
+                "C++": 7,
+                "JSON": 7,
+                "HTML": 6,
+                "CSS": 6,
+                "Matlab": 6,
+                "Java": 5,
+                "YAML": 5,
+                "Scheme (Lisp)": 5,
+                "C": 4,
+                "Jinja": 4,
+                "Lua": 3,
+                "RISC-V": 2,
+            }
         }
-    }
-    xobj.send(null);
-
+    ]
 }
 
-// Call to function with anonymous callback
-loadJSON(function(response) {
-    // Do Something with the response e.g.
-    jsonresponse = JSON.parse(response);
+console.log(default_args);
 
-    // Assuming json data is wrapped in square brackets as Drew suggests
-    console.log(jsonresponse);
+// UPDATE ARGS AS NEED BE
+var args = default_args;
 
-});
+var skills = args.skills;
+for (let i = 0; i < skills.length; i++) {
+    var skill = skills[i];
+    console.log(skill);
+
+    // Sort Dictionary a la https://stackoverflow.com/questions/25500316/sort-a-dictionary-by-value-in-javascript
+    var items = Object.keys(skill.subSkills).map(function(key) {
+      return [key, skill.subSkills[key]];
+    });
+    // Sort the array based on the second element
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+
+    // Create a new array with only the first 5 items
+    items = items.slice(0, skill.limit);
+    items = items.map(function(item) {
+        return item[0]
+    });
+    console.log(items);
+
+
+    // MAKE HTML ELEMS
+    var skillDiv = document.createElement("div");
+    console.log(skillDiv);
+    var title = document.createElement("h3");
+    title.innerHTML = skill.skillCategory;
+    var content = document.createElement("a");
+    content.innerHTML = items.join(", ");
+
+    skillDiv.appendChild(title);
+    skillDiv.appendChild(content);
+
+
+
+    var skillsBox = document.getElementById("skillsbox");
+    console.log(skillsBox);
+    skillsBox.appendChild(skillDiv);
+
+
+}
