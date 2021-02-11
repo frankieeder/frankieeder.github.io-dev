b6f5ab989403f133872c78bee5ef1850aabdccf9
Frankie Eder <frankaeder@gmail.com>
2021-02-10 16:08:23 -0800

    added multiple components.

--- content.js
+++ content.js
@@ -1,8 +1,19 @@
 var CONTENT = {
-    title: "Crow's Landing II",
-    subtitle: "2020-08-13",
-    imgs: [
-        "67/DSC00378",
-        "67/DSC00384"
+    "contents": [
+        {
+            title: "Crow's Landing II",
+            subtitle: "2020-08-13",
+            imgs: [
+                "67/DSC00378",
+                "67/DSC00384"
+            ]
+        }, {
+            title: "Crow's Landing II",
+            subtitle: "2020-08-13",
+            imgs: [
+                "67/DSC00378",
+                "67/DSC00384"
+            ]
+        }
     ]
 }
