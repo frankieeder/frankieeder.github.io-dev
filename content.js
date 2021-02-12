1ba4038ecb1fee055debc18de950fa65b3f25033
Frankie Eder <frankaeder@gmail.com>
2021-02-11 22:30:13 -0800

    Style.

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 var CONTENT = {
     "contents": [
         {
+            type_photo_scrollbox: true,
             title: "Crow's Landing I",
             subtitle: "2020-08-13",
             imgs: [
@@ -8,12 +9,15 @@ var CONTENT = {
                 "67/DSC00384"
             ]
         }, {
+            type_photo_scrollbox: true,
             tags: ['frankie_eder', 'art', 'still', 'photography', 'landscape'],
             title: "Crow's Landing II",
             subtitle: "2020-08-13",
             imgs: [
                 "67/DSC00378",
-                "67/DSC00384"
+                "67/DSC00384",
+                "67/DSC00378",
+                "67/DSC00384",
             ]
         }, {
             title: "Crow's Landing III",
