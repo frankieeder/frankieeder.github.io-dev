487da007433a047b86a9a49ba0d00a8baf80e35c
Frankie Eder <frankaeder@gmail.com>
2023-01-18 14:52:59 -0800

    Updated intro copy.

--- content.js
+++ content.js
@@ -7,10 +7,9 @@ var CONTENT = {
         {
             tags: ['frankie_eder'],
             rows: [
-                {html: "<p>My name is <a href='/about'><u>Frankie Eder and I make things</u></a>.</p>"},
+                {html: "<p>My name is <a href='/about'><u>Frankie Eder and I make things</u></a>, usually at the intersection of <a href='/film'><u>film</u></a>, <a href='/art'><u>art</u></a>, and <a href='/science'><u>science</u></a>.</p>"},
                 {html: "<p>Please sign up for my <a href='/newsletter'><u>newsletter</u></a> to stay up to date on my work.</p>"},
-                {html: "<p>Highlighted works are below, and portfolios in <a href='/art'><u>art</u></a>, <a href='/science'><u>science</u></a>, and associated subcategories such as <a href='/film'><u>film</u></a> and <a href='/still'><u>photography</u></a> are available via sidebar navigation.</p>"},
-                {html: "<p>All photos are available for purchase via clicking on any photo.</p>"}
+                {html: "<p>Photos available for purchase by clicking on any thumbnail.</p>"}
             ],
         },
         {
